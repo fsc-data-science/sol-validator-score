d0852acbd4875a0d0c11a176a32992dbeece02d4
Carlos R. Mercado <107061601+charlieflipside@users.noreply.github.com>
2023-06-15 08:56:40 -0400

    voter metrics/data

--- sol-vscore/004_validator_stake_metrics.R
+++ sol-vscore/004_validator_stake_metrics.R
@@ -1,14 +1,45 @@
 library(dplyr)
-library(plotly)
 
 # for testing pass to metric functions 
 validator_stake <- readRDS("003_latest_save.rds")
 
 # Functions ---- 
 
+rolling_absum <- function(v, k = 10){
+  d = rep(0, length(v))
+  
+  if(length(v) < (k+1)){
+  return(d)  
+  }
+  
+  for(i in k:length(v)){
+    d[i] <- sum( abs(v[ (i-k+1):i ]))
+  }
+  return(d)
+}
+
+get_10epoch_staker_churn <- function(validator_stake){
+  validator_stake %>% group_by(voter_pubkey) %>% 
+    dplyr::arrange(epoch) %>% 
+    mutate(
+      ten_epoch_staker_churn_percent = 100*rolling_absum(num_new_stakers, k = 10)/nstakers
+    )
+}
+
+get_10epoch_sol_churn <- function(validator_stake){
+  validator_stake %>% group_by(voter_pubkey) %>% 
+    dplyr::arrange(epoch) %>% 
+    mutate(
+      ten_epoch_churn_percent = 100*rolling_absum(num_new_sol_staked, k = 10)/sol_staked
+    )
+}
 
 
 # Reference Objects ----
+
+validator_10epoch_staker_churn <- get_10epoch_staker_churn(validator_stake)
+validator_10epoch_sol_stake_churn <- get_10epoch_sol_churn(validator_stake)
+
 
 
 
--- sol-vscore/006_validator_vote_metrics.R
+++ sol-vscore/006_validator_vote_metrics.R
@@ -1,11 +1,47 @@
 library(dplyr)
-library(plotly)
 
 # for testing pass to metric functions 
 validator_vote <- readRDS("005_latest_save.rds")
 
 # Functions ---- 
+rolling_avg <- function(v, k = 10, default = 100){
+  d = rep(default, length(v))
+  
+  v[is.na(v)] <- default
+  
+  if(length(v) < (k+1)){
+    return(d)  
+  }
+  
+  for(i in k:length(v)){
+    d[i] <- mean(v[ (i-k+1):i ])
+  }
+  return(d)
+}
+
+get_vote_avg10_attendance <- function(validator_vote){
+  validator_vote %>% 
+    group_by(voter_pubkey) %>%
+    arrange(epoch) %>% 
+    mutate(
+      avg_10epoch_attendance = 100 - rolling_avg(percent_slots_skipped_in_epoch, k = 10)
+    )
+}
+
+get_vote_profile <- function(validator_vote){
+  validator_vote %>% 
+    group_by(voter_pubkey) %>%
+    arrange(epoch) %>% 
+    summarise(
+      earliest_epoch = min(last_active_epoch),
+      latest_active_epoch = max(last_active_epoch),
+      age = max(last_active_epoch) - min(last_active_epoch),
+      experience = sum(active_category == "active")
+    )
+  
+}
 
 # Reference Objects ----
 
-
+vote_rolling_attendance <- get_vote_avg10_attendance(validator_vote)
+vote_profile <- get_vote_profile(validator_vote)
